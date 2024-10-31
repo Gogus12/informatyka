74cc14d314c6fb180534d880130aa8a09ab8930a
Gogus12 <aleksander.gogarowski09@gmail.com>
2024-10-31 09:16:37 +0100

    Komentarz

---
+++ lekcja 1/BezNazwy4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+   int main(){
+   	int a  ;
+   	cout <<"Podaj Wiek:";
+   	cin >>a;
+   	
+   	string b ;
+   	cout <<"Podaj imie:";
+   	cin >>b;
+   	
+   	cout<< "WItaj "<<b << "masz"<<a;
+   }
+	
+	
