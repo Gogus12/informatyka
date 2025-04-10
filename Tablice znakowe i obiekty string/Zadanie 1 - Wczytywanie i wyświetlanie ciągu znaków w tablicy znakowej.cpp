012321396fe2c276dd7c5b333f41bfc35657600a
Gogus12 <aleksander.gogarowski09@gmail.com>
2025-04-10 09:35:58 +0200

    Komentarz

--- Tablice znakowe i obiekty string/Zadanie 1 - Wczytywanie i wyświetlanie ciągu znaków w tablicy znakowej.cpp
+++
@@ -1,8 +0,0 @@
- #include <iostream>
- using namespace std;
- 	int main(){
- 		char ciag[21];
-	cout << "Wprowadz ciag znakow (max 20 znakow): ";
-    cin.getline(ciag, 21); 
-	cout << "Wprowadzony ciag znakow: " << ciag << endl;
-}
